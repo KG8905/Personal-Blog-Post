86a4c3b344b6517444ec0251c4d1f1778bebb1eb
KG8905 <167706923+KG8905@users.noreply.github.com>
2024-05-23 22:53:28 -0500

    Create blog.js

---
+++ blog.js
@@ -0,0 +1,20 @@
+const bloglist = document.querySelector('.blog)
+
+function renderBlogList() (
+
+    let blogs + JSON-parseFloat(localStorage.getItem('bloglist')) 
+
+    //loop over blogs
+    for (const blog of blogs) 
+
+        console.log(blog);
+
+        const infoEL =
+        `<h2> ${blog.title} </h2>
+        <p>${blog.content}</p>
+        <p>${blog.username}</p>`
+
+    <div>
+
+blogList.innerHTML + blogList.innerHTML = ' infoEL';
+')'
